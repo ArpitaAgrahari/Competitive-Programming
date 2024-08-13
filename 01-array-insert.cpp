67f6081b704c67e4703b1ae9148b20743c875bec
arpita <arpitaagrahari004@gmail.com>
2024-08-13 11:00:29 +0530

    feat: Add insertion on nth pos and first pos in array
    This commit adds the functionality to insert an element at a specific position in an array. It includes two separate functions: one for inserting at the nth position and another for inserting at the first position. The code has been tested and is working as expected.

--- 01-array-insert.cpp
+++ 01-array-insert.cpp
@@ -1,4 +1,4 @@
-//insertion
+//insertion on nth pos
 //1,2,3,4,5,--23--,6,7,8,9
 
 #include<iostream>
@@ -15,6 +15,27 @@ int main(){
     int n = 10;
 
     insert(arr, &n,5,23);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
+
+
+// insertion on  first pos
+#include<iostream>
+using namespace std;
+void insert(int arr[],int *n,int pos,int key){
+    (*n)++;
+    for(int i=(*n-1);i>=pos;i--){
+        arr[i] = arr[i-1];
+    }
+    arr[pos-1] = key;
+}
+int main(){
+    int arr[100] = {1,2,3,4,5,6,7,8,9,10};
+    int n = 10;
+
+    insert(arr, &n,1,23);
     for(int i=0;i<n;i++){
         cout<<arr[i]<<" ";
     }
