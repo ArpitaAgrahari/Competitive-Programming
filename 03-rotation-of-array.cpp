5e8dc0836b93a394477e1883a3f21eccce87cf22
arpita <arpitaagrahari004@gmail.com>
2024-08-13 11:36:57 +0530

    feat: Implement left rotation of array
    This commit implements the left rotation of an array. It uses the given index to rotate the array elements to the left. The code has been tested and is working as expected.

---
+++ 03-rotation-of-array.cpp
@@ -0,0 +1,24 @@
+//left rotation
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[] = {1,2,3,4,5,6,7,8,9,10};
+    int n=10;
+    int index =3;
+    //if index is too targensince arr is too small we will reduce the size(k=1000, so we wil do k=k%n);
+    //1st approach to divide the array in n-k element in array and left element in array and then insert it
+    //auxiliary array
+    int a[10];
+    int k=0;
+    for(int i=index;i<n;i++){
+        a[k] = arr[i];
+        k++;
+    }
+    for(int i=0;i<index;i++){
+        a[k] = arr[i];
+        k++;
+    }
+    for(int i=0;i<n;i++){
+        cout<<a[i]<<" ";
+    }
+}
