148b35635fe497a6ce71dbbd7200c96c8ba31d18
arpita <arpitaagrahari004@gmail.com>
2024-08-13 11:15:10 +0530

    feat: Add insertion functions for array

--- 01-array-insert.cpp
+++ 01-array-insert.cpp
@@ -42,14 +42,4 @@ int main(){
 }
 
 
-#include<iostream>
-using namespace std;
-void dele
-int main(){ 
-    int array[100];
-    int n = 10;
-    for(int i=0;i<n;i++){
-        cin>>array[i];
-    }
-    
-}
+
