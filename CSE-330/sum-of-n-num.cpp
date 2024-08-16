4bf07f861bf3e1dddae35adaa82d9b22e1a0f1df
arpita <arpitaagrahari004@gmail.com>
2024-08-16 16:17:21 +0530

    feat: Refactor array deletion logic and add insertion functions

--- CSE-330/sum-of-n-num.cpp
+++ CSE-330/sum-of-n-num.cpp
@@ -1,11 +1,14 @@
+//sum of n natural numbers using for loop
 #include<iostream>
 using namespace std;
 int main(){
     int n;
     cin>>n;
     int sum = 0;
-    for(int i=0;i<n;i++){
-        sum = sum+i;
+    for(int i=1;i<=n;i++){
+        sum+=i;
     }
-    cout<<sum<<endl;
+    cout<<sum;
 }
+
+//sum using formual
