1686974d682ea05f0c65942b55c7f7c5e819dbb5
arpita <arpitaagrahari004@gmail.com>
2024-08-20 11:21:37 +0530

    chore: Remove commented code and fix incomplete array declaration in lower-triangle.cpp

--- lower-triangle.cpp
+++ lower-triangle.cpp
@@ -1,20 +1,28 @@
 
 //lower triangle in matrix
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int arr[][3] = {{1,2,3},{4,5,6},{7,8,9}};
+//     for(int i=0;i<3;i++){
+//         for(int j=0;j<3;j++){
+//             if(i<=j){
+//                 arr[i][j]=0;
+//             }
+//         }
+//     }
+//     for(int i=0;i<3;i++){
+//         for(int j=0;j<3;j++){
+//             cout<<arr[i][j]<<" ";
+//             cout<<endl;
+//         }
+//     }
+// }
+
+
+//finding the peak element
 #include<iostream>
 using namespace std;
 int main(){
-    int arr[][3] = {{1,2,3},{4,5,6},{7,8,9}};
-    for(int i=0;i<3;i++){
-        for(int j=0;j<3;j++){
-            if(i<=j){
-                arr[i][j]=0;
-            }
-        }
-    }
-    for(int i=0;i<3;i++){
-        for(int j=0;j<3;j++){
-            cout<<arr[i][j]<<" ";
-            cout<<endl;
-        }
-    }
+    
 }
