6e44c53b39e5eef13a6b38c0fc6bbff657a15bd6
arpita <arpitaagrahari004@gmail.com>
2024-08-20 11:19:39 +0530

    chore: Fix incomplete array declaration in lower-triangle.cpp

--- lower-triangle.cpp
+++ lower-triangle.cpp
@@ -1,12 +1,20 @@
+
+//lower triangle in matrix
 #include<iostream>
 using namespace std;
 int main(){
-    int arr[][] = {{1,2,3},{4,5,6},{7,8,9}};
+    int arr[][3] = {{1,2,3},{4,5,6},{7,8,9}};
     for(int i=0;i<3;i++){
         for(int j=0;j<3;j++){
-            if(i>=j){
+            if(i<=j){
                 arr[i][j]=0;
             }
+        }
+    }
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            cout<<arr[i][j]<<" ";
+            cout<<endl;
         }
     }
 }
