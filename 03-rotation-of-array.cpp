2a7daa7d4d187a0c624874300b747a88023d799f
arpita <arpitaagrahari004@gmail.com>
2024-08-20 10:34:18 +0530

    Different approach for right rotation

--- 03-rotation-of-array.cpp
+++ 03-rotation-of-array.cpp
@@ -59,27 +59,27 @@
 
 //2nd approach :( )
 
-#include<iostream>
-using namespace std;
-int main(){
-    int arr[] = {1,2,3,4,5,6,7,8,9,10};
-    int n=10;
-    int r = 3;
-    r =r %n;
-    for(int i=0,j=(n-r-1);i<j;j--){
-        int t = arr[i];
-        arr[i] = arr[j];
-        arr[j] = t;
-    }
-    for(int i=n-r,j=n-1);i<j;i++,j--){
-        int t = arr[i];
-        arr[i] = arr[j];
-        arr[j] = t;
-    }
-    for(int i=0,j=(n-r-1);i<j;j--){
-        int t = arr[i];
-        arr[i] = arr[j];
-        arr[j] = t;
-    }
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int arr[] = {1,2,3,4,5,6,7,8,9,10};
+//     int n=10;
+//     int r = 3;
+//     r =r %n;
+//     for(int i=0,j=(n-r-1);i<j;j--){
+//         int t = arr[i];
+//         arr[i] = arr[j];
+//         arr[j] = t;
+//     }
+//     for(int i=n-r,j=n-1);i<j;i++,j--){
+//         int t = arr[i];
+//         arr[i] = arr[j];
+//         arr[j] = t;
+//     }
+//     for(int i=0,j=(n-r-1);i<j;j--){
+//         int t = arr[i];
+//         arr[i] = arr[j];
+//         arr[j] = t;
+//     }
 
-}
+// }
