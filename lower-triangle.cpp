10b1b4a72f702050846ceec8c4559b0ec42dd69f
arpita <arpitaagrahari004@gmail.com>
2024-08-20 11:29:54 +0530

    chore: Remove commented code and fix incomplete array declaration in lower-triangle.cpp

--- lower-triangle.cpp
+++ lower-triangle.cpp
@@ -21,8 +21,22 @@
 
 
 //finding the peak element
-#include<iostream>
-using namespace std;
-int main(){
-    
-}
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int arr[]={12,10,5,7,6,2};
+//     int n = 6;
+//      if(arr[0]>arr[1]){
+//             cout<<arr[0]<<" ";
+//         }else{
+//     for(int i=0;i<n;i++){
+       
+//         if(arr[i]>arr[i-1] && arr[i]>arr[i+1]){
+//             cout<<arr[i]<<" ";
+//         }
+//     }
+//     }
+// }
+
+
+//finding peak using binary search
