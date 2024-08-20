4e246a9e1faa3bedc4a7ff27d07930b94f4e1905
arpita <arpitaagrahari004@gmail.com>
2024-08-20 10:53:06 +0530

    chore: Refactor matrix multiplication code for improved efficiency and readability

--- matrix.cpp
+++ matrix.cpp
@@ -14,19 +14,33 @@
 
 #include<iostream>
 using namespace std;
+
+void input(int arr[],int r,int c){
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            cin>>arr[i][j];
+        }
+    }
+}
 int main(){
     int r1,c1,r2,c2;
     cin>>r1>>c1>>r2>>c2;
     int arr1[r1][c1];
     int arr2[r2][c2];
 
-    int arr3[r1][c2];
-    if(c1!=r2){
-        return;
-    }
+    int arr3[r1][c2]={0};
+    // if(c1!=r2){
+    //     return 0;
+    // }
+
+    input(arr1,r1,c1);
+    input(arr2,r2,c2);
+
     for(int i=0;i<r1;i++){
         for(int j=0;i<c2;j++){
-            cout<<arr3[i][j]<<" ";
+            for(int k=0;k<c1;k++){
+                arr3[i][j]=arr3[i][j]+arr1[i][k]*arr2[k][j];
+            }
         }
     }
 }
