ff142b57a0001de7102e780f0c2a4984d98a4811
arpita <arpitaagrahari004@gmail.com>
2024-08-21 14:21:40 +0530

    Refactor prime-num.cpp to improve prime number checking logic

--- CSE-330/prime-num.cpp
+++ CSE-330/prime-num.cpp
@@ -1,7 +1,20 @@
-#include <iostream>
-using namespace std;
 
-int main() {
-    
-    return 0;
+//naive approach to check prime or not 
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    bool flag = 0;
+    for(int i=2;i<n;i++){
+        if(n%i==0){
+            cout<<"Non prime";
+            flag = 1;
+            break;
+        }
+    }
+    if(flag==0){
+        cout<<"Prime";
+
+    }
 }
