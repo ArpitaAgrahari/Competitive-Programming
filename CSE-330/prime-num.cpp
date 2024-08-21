8521c7eef02587fbeaa4c952c86d09b3d2fdd6c0
arpita <arpitaagrahari004@gmail.com>
2024-08-21 14:27:13 +0530

    Refactor prime-num.cpp to use efficient approach for checking prime numbers

--- CSE-330/prime-num.cpp
+++ CSE-330/prime-num.cpp
@@ -1,14 +1,33 @@
 
 //naive approach to check prime or not 
 
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int n;
+//     cin>>n;
+//     bool flag = 0;
+//     for(int i=2;i<n;i++){
+//         if(n%i==0){
+//             cout<<"Non-prime";
+//             flag = 1;
+//             break;
+//         }
+//         }if(flag==0){
+//             cout<<"Prime";
+//         }
+//     return 0;
+// }
+
+//efficent approach to check prime using square root of n
 #include <iostream>
 using namespace std;
-
 int main() {
     int n;
     cin>>n;
     bool flag = 0;
-    for(int i=2;i<n;i++){
+    for(int i=2;i*i<=n;i++){
         if(n%i==0){
             cout<<"Non-prime";
             flag = 1;
@@ -19,5 +38,3 @@ int main() {
         }
     return 0;
 }
-
-//efficent approach to check prime using square root of n
