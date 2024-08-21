56107a6ab945b13619f2a924226f8771a8c5b8e4
arpita <arpitaagrahari004@gmail.com>
2024-08-21 14:29:40 +0530

    Refactor prime-num.cpp to improve prime number checking logic

--- CSE-330/prime-num.cpp
+++ CSE-330/prime-num.cpp
@@ -29,8 +29,7 @@ int main() {
     int n;
     cin>>n;
     bool flag = 0;
-    for(int i=2;i*i<=n;i++){        //i*i<=n is the condition how is it working?
-                                    //because if n is not prime then it will have atleast one factor less than or equal to square root of n
+    for(int i=2;i*i<=n;i++){        
         if(n%i==0){
             cout<<"Non-prime";
             flag = 1;
