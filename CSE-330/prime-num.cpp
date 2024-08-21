0c60d476cd37681e676c503f9e736b5be8593a08
arpita <arpitaagrahari004@gmail.com>
2024-08-21 14:30:14 +0530

    Refactor prime-num.cpp to fix prime number checking logic

--- CSE-330/prime-num.cpp
+++ CSE-330/prime-num.cpp
@@ -31,12 +31,12 @@ int main() {
     bool flag = 0;
     for(int i=2;i*i<=n;i++){        
         if(n%i==0){
-            cout<<"Non-prime";
+            cout<<"non-prime";
             flag = 1;
             break;
         }
         }if(flag==0){
-            cout<<"Prime";
+            cout<<"prime";
     }
     return 0;
 }
