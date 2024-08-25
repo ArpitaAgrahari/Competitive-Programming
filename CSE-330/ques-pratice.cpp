27f94ad6bb61e6ee829b120a798cdeaeffdb6c65
arpita <arpitaagrahari004@gmail.com>
2024-08-25 12:41:22 +0530

    Refactor matrix-setting.cpp to optimize row and column setting logic

--- CSE-330/ques-pratice.cpp
+++ CSE-330/ques-pratice.cpp
@@ -11,8 +11,7 @@ int main(){
     int matrix[n][n];
     cout<<"Enter the elements of the matrix: ";
     for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            cin>>matrix[i][j];
+        for(int j=0;j<n;j++){cin>>matrix[i][j];
         }
     }
     int row[n]={0};
