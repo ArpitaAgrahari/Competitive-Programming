1d43cd984064524e89eb300ca55b8efedc3dba22
arpita <arpitaagrahari004@gmail.com>
2024-08-27 10:21:30 +0530

    Refactor freq-of-string.cpp for improved efficiency in finding frequency of s2 in s1

--- freq-of-string.cpp
+++ freq-of-string.cpp
@@ -8,12 +8,12 @@ int main() {
     int count=0;
     for(int i=0;i<s1.length();i++){
         if(s1[i]==s2[0]){
-            int j=0;
-            while(j<s2.length() && s1[i+j]==s2[j]){
-                j++;
-            }
-            if(j==s2.length()){
-                count++;
+        int j=0;
+        while(j<s2.length() && s1[i+j]==s2[j]){
+            j++;
+        }
+        if(j==s2.length()){
+            count++;
             }
         }
     }
