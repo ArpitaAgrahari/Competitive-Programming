ba7f49ad952270619fa2e8beccbbab8a6528daa2
arpita <arpitaagrahari004@gmail.com>
2024-08-27 10:26:12 +0530

    Refactor freq-of-string.cpp for improved efficiency in finding frequency of s2 in s1

--- freq-of-string.cpp
+++ freq-of-string.cpp
@@ -1,22 +1,42 @@
 //given two string and s 1 and s2 you have to find the frequency of s2 in s1.
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     string s1,s2;
+//     cin>>s1>>s2;
+//     int count=0;
+//     for(int i=0;i<s1.length();i++){
+//         if(s1[i]==s2[0]){
+//         int j=0;
+//         while(j<s2.length() && s1[i+j]==s2[j]){
+//             j++;
+//         }
+//         if(j==s2.length()){
+//             count++;
+//             }
+//         }
+//     }
+//     cout<<count;
+//     return 0;
+// }
+
+//to check how many alphabet is not present in the string s1
 #include <iostream>
 using namespace std;
-
 int main() {
-    string s1,s2;
-    cin>>s1>>s2;
+    string s1;
+    cin>>s1;
     int count=0;
+    int arr[26]={0};
     for(int i=0;i<s1.length();i++){
-        if(s1[i]==s2[0]){
-        int j=0;
-        while(j<s2.length() && s1[i+j]==s2[j]){
-            j++;
-        }
-        if(j==s2.length()){
+        arr[s1[i]-'a']=1;
+    }
+    for(int i=0;i<26;i++){
+        if(arr[i]==0){
             count++;
-            }
         }
     }
     cout<<count;
     return 0;
-}
+} 
