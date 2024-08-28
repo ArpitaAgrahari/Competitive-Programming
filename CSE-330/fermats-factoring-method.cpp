9799bba751bfb853df567e4a57b831535ad0c8a6
arpita <arpitaagrahari004@gmail.com>
2024-08-28 14:42:14 +0530

    Refactor fermat's-factoring-method.cpp for improved efficiency in factoring composite numbers

--- CSE-330/fermats-factoring-method.cpp
+++ CSE-330/fermats-factoring-method.cpp
@@ -11,10 +11,10 @@ int main(){
     }
     int a = ceil(sqrt(n));
     int b2 = a*a - n;
-        while (static_cast<int>(sqrt(b2)) * static_cast<int>(sqrt(b2)) != b2) {
+    while (static_cast<int>(sqrt(b2)) * static_cast<int>(sqrt(b2)) != b2) {
             a++;
             b2 = a * a - n;
-}
+        }
 
     int b = sqrt(b2);
     int factor1 = a+b;
