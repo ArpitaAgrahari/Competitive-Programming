76a4315533ee87f994c4c4ca6dff3d96ef41de75
arpita <arpitaagrahari004@gmail.com>
2024-08-28 14:37:12 +0530

    Refactor fermat's-factoring-method.cpp for improved efficiency in factoring composite numbers

--- CSE-330/fermat's-factoring-method.cpp
+++ CSE-330/fermat's-factoring-method.cpp
@@ -1,15 +1,23 @@
 //fermat's factoring method  : used to factorize a composite number into its prime factors
 #include<iostream>
+#include<cmath>
 using namespace std;
 int main(){
     int n;
     cin>>n;
-    int a = sqrt(n);
-    int b = a*a - n;
-    while(sqrt(b)!=int(sqrt(b))){
-        a++;
-        b = a*a - n;
+    if(n<=0){
+        cout<<"Invalid input";
+        return 0;
     }
-    cout<<a-sqrt(b);
+    int a = ceil(sqrt(n));
+    int b2 = a*a - n;
+    while(sqrt(b2) != int(sqrt(b2))){
+        a++;
+        b2 = a*a - n;
+    }
+    int b = sqrt(b2);
+    int factor1 = a+b;
+    int factor2 = a-b;
+    cout<<factor1<<" "<<factor2;
     return 0;
 }
