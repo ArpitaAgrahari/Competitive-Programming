7fe26b8e8a60d2a6548457d96ff3d101982da6f5
arpita <arpitaagrahari004@gmail.com>
2024-09-03 10:42:18 +0530

    Implemnetation of push and pop using linked list

--- stack-using-LL.cpp
+++ stack-using-LL.cpp
@@ -6,5 +6,34 @@ struct node{
     int data;
     struct node *next;
 };
-
+void push(struct node** top,int key){
+    struct node* temp=new node;
+    temp->data=key;
+    temp->next=*top;
+    *top=temp;
+}
+void pop(struct node** top){
+    if(*top==NULL)
+    return;
+    struct node* temp=*top;
+    *top=(*top)->next;
+    delete temp;
+}
+void print_stack(struct node* top){
+    while(top!=NULL){
+        cout<<top->data<<" ";
+        top=top->next;
+    }
+}
+int main(){
+    node* top=NULL;
+    push(&top,10);
+    push(&top,20);
+    push(&top,30);
+    push(&top,40);
+    push(&top,50);
+    pop(&top);
+    print_stack(top);
+    return 0;
+}
 
