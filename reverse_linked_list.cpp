fda44c3d7238d1fd84b12963aee1a55268f445f0
arpita <arpitaagrahari004@gmail.com>
2024-09-03 11:06:43 +0530

    Refactor reverse_linked_list.cpp for improved readability and maintainability

--- reverse_linked_list.cpp
+++ reverse_linked_list.cpp
@@ -18,9 +18,32 @@ void reverse_linked_list(struct node** first){
     }
     *first=prev;
 }
+
+struct node* insert_at_first(struct node* first,int key){
+    struct node* temp=new node;
+    temp->data=key;
+    temp->next=NULL;
+    if(first==NULL)
+    return temp;
+    temp->next=first;
+    return temp;
+}
 void print_linked_list(struct node* first){
     while(first!=NULL){
         cout<<first->data<<" ";
         first=first->next;
     }
+}
+
+int main(){
+    node* first=NULL;
+    first=insert_at_first(first,10);
+    first=insert_at_first(first,20);
+    first=insert_at_first(first,30);
+    first=insert_at_first(first,40);
+    first=insert_at_first(first,50);
+    first=insert_at_first(first, 60);
+    reverse_linked_list(&first);
+    print_linked_list(first);
+    return 0;
 }
