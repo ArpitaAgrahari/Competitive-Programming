6d8ad474b975a82154538828f1f3f6a2dd77c392
arpita <arpitaagrahari004@gmail.com>
2024-09-03 10:23:28 +0530

    Refactor Linked-list.cpp for improved readability and maintainability

--- Linked-list.cpp
+++ Linked-list.cpp
@@ -3,8 +3,29 @@ using namespace std;
 
 struct node{
     int data;
-    struct node* next;
+    struct node *next;
+};
+void print_Linked_List(struct node* first){
+    while(first!=NULL){
+        cout<<first->data<<" ";
+        first=first->next;
+    }
+}
+struct node* insert_at_first(struct node* first,int key){
+    struct node* temp=new node;
+    temp->data=key;
+    temp->next=NULL;
+    if(first==NULL)
+    return temp;
+    temp->next=first;
+    return temp;
 }
 int main(){
     node* first=NULL;
+    first=insert_at_first(first,10);
+    first=insert_at_first(first,20);
+    first=insert_at_first(first,30);
+    first=insert_at_first(first,40);
+    first=insert_at_first(first,50);
+    print_Linked_List(first);
 }
