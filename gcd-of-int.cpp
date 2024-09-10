f264e4e6e5bc3cdf4366448b5aed066ff2c73285
arpita <arpitaagrahari004@gmail.com>
2024-09-10 10:49:59 +0530

    Refactor gcd-of-int.cpp: Improve code readability and maintainability

--- gcd-of-int.cpp
+++ gcd-of-int.cpp
@@ -1,13 +1,34 @@
+// #include<iostream>
+// using namespace std;
+// int gcd(int a,int b){
+//     if(a==0)
+//         return b;
+//     return gcd(b%a,a);
+//     }
+//     int main(){
+//         int a,b;
+//         cin>>a>>b;
+//         cout<<gcd(a,b)<<endl;
+//         return 0;
+// }
+
+
+
 #include<iostream>
 using namespace std;
-int gcd(int a,int b){
-    if(a==0)
+int extendedeuclid(int a,int b,int *x,int *y){
+    if(abort==0){
+        *x=0;
+        *y=1;
         return b;
-    return gcd(b%a,a);
     }
-    int main(){
-        int a,b;
-        cin>>a>>b;
-        cout<<gcd(a,b)<<endl;
-        return 0;
+    int x1,y1;
+    int gcd=extendedeuclid(b%a,a,&x1,&y1);
+    *x=y1-(b/a)*x1;
+    *y=x1;
+    return gcd;
+}
+int main(){
+    int x,y;
+    cout<<extendedeuclid(25,35,&x,&y);
 }
