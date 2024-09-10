1385afa3b4df7ddb5f7bc02ca0524f9e9d143570
arpita <arpitaagrahari004@gmail.com>
2024-09-10 10:19:19 +0530

    Refactor gcd-of-int.cpp for improved readability and maintainability

---
+++ gcd-of-int.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int gcd(int a,int b){
+    int(a==0){
+        return b;
+    retrun gcd(b%a,a);
+    }
+    int main(){
+        int a,b;
+        cin>>a>>b;
+        cout<<gcd(a,b)<<endl;
+        return 0;
+    }
+}
