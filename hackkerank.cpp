19d6f06cf8aafa6090cb1cc00ee257301d9c9a9b
arpita <arpitaagrahari004@gmail.com>
2024-09-12 12:06:42 +0530

    Refactor hackkerank.cpp: Remove unnecessary code and fix formatting

--- hackkerank.cpp
+++ hackkerank.cpp
@@ -14,30 +14,30 @@
 
 // 2
 
-#include <iostream>
-using namespace std;
-int main(){
-    int n;
-    cin>>n;
-    int arr[n];
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
-    }
-    int sum=0;
-    for(int i=0;i<n;i++){
-        sum+=arr[i];
-    }
-    int leftsum=0;
-    for(int i=0;i<n;i++){
-        sum-=arr[i];
-        if(leftsum>sum){
-            cout<<i<<endl;
-            return 0;
-        }
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//     int arr[n];
+//     for(int i=0;i<n;i++){
+//         cin>>arr[i];
+//     }
+//     int sum=0;
+//     for(int i=0;i<n;i++){
+//         sum+=arr[i];
+//     }
+//     int leftsum=0;
+//     for(int i=0;i<n;i++){
+//         sum-=arr[i];
+//         if(leftsum>sum){
+//             cout<<i<<endl;
+//             return 0;
+//         }
         
-        leftsum+=arr[i];
-    }
+//         leftsum+=arr[i];
+//     }
 
-    cout<<-1<<endl;
-    return 0;
-}
+//     cout<<-1<<endl;
+//     return 0;
+// }
