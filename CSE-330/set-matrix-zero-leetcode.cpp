a75c8bde1b1648cc6bcc09b00e98a773a671b0c8
arpita <arpitaagrahari004@gmail.com>
2024-09-14 01:11:41 +0530

    Remove unnecessary output loop from set matrix zero implementation

--- CSE-330/set-matrix-zero-leetcode.cpp
+++ CSE-330/set-matrix-zero-leetcode.cpp
@@ -45,11 +45,5 @@ int main(){
             matrix[i][0] = 0;
         }
     }
-    for(int i=0;i<row;i++){
-        for(int j=0;j<col;j++){
-            cout<<matrix[i][j]<<" ";
-        }
-        cout<<endl;
-    }
     
 }
