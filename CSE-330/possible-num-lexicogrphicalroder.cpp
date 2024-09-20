91ff9315929e8b849e3d4508caedf6973d013a94
arpita <arpitaagrahari004@gmail.com>
2024-09-20 16:21:55 +0530

    Refactor code to add problem statement and instructions for lexicographical order

--- CSE-330/possible-num-lexicogrphicalroder.cpp
+++ CSE-330/possible-num-lexicogrphicalroder.cpp
@@ -1,7 +1,25 @@
-//Problem Statement:  You are given a number n,print all possibke numbers from 1-n in lexicographical order.
+//Problem Statement:  You are given a number n,print all possible numbers from 1-n in lexicographical order using prefix and recursove call.
 
-#include<iostream>
-#include<string>
+#include <iostream>
 using namespace std;
-int main(){
+void check(int curr, int n) {
+    if (curr > n) return;
+    cout << curr << " ";
+    for (int i = 0; i <= 9; ++i) {
+        int next = curr * 10 + i;
+        if (next > n) return;
+        check(next, n);
+    }
+}
+void printorder(int n) {
+    for (int i = 1; i <= 9; ++i) {
+        check(i, n);
+    }
+}
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    printorder(n);
+    return 0;
 }
