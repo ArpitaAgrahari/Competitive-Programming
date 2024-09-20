d23c20281c41e1ce146d7238b0754608cfc5bbd0
arpita <arpitaagrahari004@gmail.com>
2024-09-20 15:34:37 +0530

    Refactor code to remove unnecessary includes and commented out sections

--- CSE-330/Sum-of-substr.cpp
+++ CSE-330/Sum-of-substr.cpp
@@ -1,30 +1,30 @@
 //Problem statement : given a array print all the sum of the subset genrated from it, in the increasing order.
 
-// #include<iostream>
-// #include<vector>
-// #include <algorithm>
-// using namespace std;
+#include<iostream>
+#include<vector>
+#include <algorithm>
+using namespace std;
 
-// void Sums(const vector<int>& arr, int index, int curr, vector<int>& sub) {
-//     if (index == arr.size()) {
-//         sub.push_back(curr);
-//         return;
-//     }
-//     Sums(arr, index + 1, curr + arr[index], sub);
-//     Sums(arr, index + 1, curr, sub);
-// }
+void Sums(const vector<int>& arr, int index, int curr, vector<int>& sub) {
+    if (index == arr.size()) {
+        sub.push_back(curr);
+        return;
+    }
+    Sums(arr, index + 1, curr + arr[index], sub);
+    Sums(arr, index + 1, curr, sub);
+}
 
-// int main() {
-//     vector<int> arr = {1, 2, 3};
-//     vector<int> sub;
-//     Sums(arr, 0, 0, sub);
-//     sort(sub.begin(), sub.end());
-//     for (int sum : sub) {
-//         cout << sum << " ";
-//     }
-//     cout << endl;
-//     return 0;
-// }
+int main() {
+    vector<int> arr = {1, 2, 3};
+    vector<int> sub;
+    Sums(arr, 0, 0, sub);
+    sort(sub.begin(), sub.end());
+    for (int sum : sub) {
+        cout << sum << " ";
+    }
+    cout << endl;
+    return 0;
+}
 
 
 
