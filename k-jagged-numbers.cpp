42d2639ec0b507a461089c9e540774b113ba37f8
arpita <arpitaagrahari004@gmail.com>
2024-09-27 13:23:17 +0530

    Refactor code to add k-jagged numbers implementation

--- k-jagged-numbers.cpp
+++ k-jagged-numbers.cpp
@@ -3,3 +3,20 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int> spf(n+1,0);
+    for(int i=2;i<=n;i++) {
+        if(spf[i]==0) {
+            for(int j=i;j<=n;j+=i) {
+                if(spf[j]==0) spf[j]=i;
+            }
+        }
+    }
+    for(int i=2;i<=n;i++) {
+        if(spf[i]>=5) cout<<i<<" ";
+    }
+    return 0;
+}
