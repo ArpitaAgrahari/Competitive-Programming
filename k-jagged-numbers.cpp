6ee824af801882924496adeab5122ad61e725995
arpita <arpitaagrahari004@gmail.com>
2024-09-27 13:26:12 +0530

    Refactor code to fix formatting and add missing newline at the end of file

--- k-jagged-numbers.cpp
+++ k-jagged-numbers.cpp
@@ -1,22 +1,23 @@
 //k-jagged numbers: The number whose smallest prime factor is greater than or equal to the given k, it is called k-rough or k-jagged number.
 
-#include <iostream>
-#include <vector>
+//k-jagged numberz:
+#include<iostream>
+#include<vector>
 using namespace std;
-
 int main(){
     int n;
     cin>>n;
-    vector<int> spf(n+1,0);
-    for(int i=2;i<=n;i++) {
-        if(spf[i]==0) {
-            for(int j=i;j<=n;j+=i) {
-                if(spf[j]==0) spf[j]=i;
+    vector<int> vt(n+1,0);
+    for(int i=2;i<=n;i++){
+        if(vt[i]==0){
+            for(int j=i;j<=n;j+=i){
+                if(vt[j]==0) vt[j]=i;
             }
         }
     }
-    for(int i=2;i<=n;i++) {
-        if(spf[i]>=5) cout<<i<<" ";
+    for(int i=2;i<=n;i++){
+        if(vt[i]>=5) cout<<i<<" ";
     }
     return 0;
+
 }
