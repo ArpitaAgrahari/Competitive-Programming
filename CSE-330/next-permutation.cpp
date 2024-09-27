a331ecc3af17874a4403efbb2f0ce307788731b0
arpita <arpitaagrahari004@gmail.com>
2024-09-27 16:27:12 +0530

    Refactor code to implement the next permutation algorithm

---
+++ CSE-330/next-permutation.cpp
@@ -0,0 +1,36 @@
+// next permutation :  
+
+#include <iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int next_permutation(vector<int> &v){
+    int n=v.size();
+    int i=n-2;
+    while(i>=0 && v[i]>=v[i+1]){
+        i--;
+    }
+    if(i<0){
+        return 0;
+    }
+    int j=n-1;
+    while(v[j]<=v[i]){
+        j--;
+    }
+    swap(v[i],v[j]);
+    reverse(v.begin()+i+1,v.end());
+    return 1;
+}
+int main(){
+    int n;
+    cin>>n;
+    vector<int> v(n);
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+    next_permutation(v.begin(),v.end());
+    for(int i=0;i<n;i++){
+        cout<<v[i]<<" ";
+    }
+    return 0;
+}
