a190ca85f015b518468e90ca4dda42ee6b073ac6
arpita <arpitaagrahari004@gmail.com>
2024-10-11 16:25:47 +0530

    Refactor code to implement the knapsack problem algorithm

--- CSE-330/knapsack.cpp
+++ CSE-330/knapsack.cpp
@@ -1,34 +1,53 @@
-//knapsack probelm 
 #include <iostream>
 #include <vector>
 #include <algorithm>
 using namespace std;
-int knapsack(int n,int w,vector<int> &wt,vector<int> &val){
-    vector<vector<int>> dp(n+1,vector<int>(w+1,0));
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=w;j++){
-            if(wt[i-1]<=j){
-                dp[i][j]=max(val[i-1]+dp[i-1][j-wt[i-1]],dp[i-1][j]);
-            }
-            else{
-                dp[i][j]=dp[i-1][j];
+
+int knapsack(int n, int w, vector<int> &wt, vector<int> &val) {
+    vector<vector<int>> dp(n + 1, vector<int>(w + 1, 0));
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= w; j++) {
+            if (wt[i - 1] <= j) {
+                dp[i][j] = max(val[i - 1] + dp[i - 1][j - wt[i - 1]], dp[i - 1][j]);
+            } else {
+                dp[i][j] = dp[i - 1][j];
             }
         }
     }
     return dp[n][w];
 }
-int main(){
-    int n,w;
-    cin>>n>>w;
-    vector<int> wt(n),val(n);
-    for(int i=0;i<n;i++){
-        cin>>wt[i];
+
+int main() {
+    int n, w;
+    cout << "Enter the number of items and the maximum weight: ";
+    cin >> n >> w;
+
+    if (n <= 0 || w <= 0) {
+        cout << "Invalid input. Number of items and maximum weight must be positive." << endl;
+        return 1;
     }
-    for(int i=0;i<n;i++){
-        cin>>val[i];
+
+    vector<int> wt(n), val(n);
+    cout << "Enter the weights of the items: ";
+    for (int i = 0; i < n; i++) {
+        cin >> wt[i];
+        if (wt[i] <= 0) {
+            cout << "Invalid input. Weights must be positive." << endl;
+            return 1;
+        }
     }
-    cout<<knapsack(n,w,wt,val);
+
+    cout << "Enter the values of the items: ";
+    for (int i = 0; i < n; i++) {
+        cin >> val[i];
+        if (val[i] <= 0) {
+            cout << "Invalid input. Values must be positive." << endl;
+            return 1;
+        }
+    }
+
+    int max_value = knapsack(n, w, wt, val);
+    cout << "The maximum value that can be obtained is: " << max_value << endl;
+
     return 0;
 }
-//time complexity : O(n*w)
-//space complexity : O(n*w)
