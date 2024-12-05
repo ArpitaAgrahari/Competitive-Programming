005577a9700236f9c035b7827e8f8011429c13b9
arpita <arpitaagrahari004@gmail.com>
2024-12-05 23:23:43 +0530

    Remove unnecessary blank line in LCS function implementation in ques.cpp

--- etp/ques.cpp
+++ etp/ques.cpp
@@ -7,7 +7,6 @@ int LCS(string str1, string str2) {
     int n = str1.size();
     int m = str2.size();
     vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
-
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= m; j++) {
             if (str1[i - 1] == str2[j - 1]) {
