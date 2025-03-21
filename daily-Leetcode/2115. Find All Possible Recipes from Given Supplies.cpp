ccb536cbc166dcde0e97c85d2eeda2bcf4980365
Arpita Agrahari <arpitaagrahari004@gmail.com>
2025-03-21 23:33:42 +0530

    Add solution for finding all possible recipes from given supplies

---
+++ daily-Leetcode/2115. Find All Possible Recipes from Given Supplies.cpp
@@ -0,0 +1,52 @@
+class Solution
+{
+public:
+    vector<string> findAllRecipes(vector<string> &recipes, vector<vector<string>> &ingredients, vector<string> &supplies)
+    {
+        unordered_set<string> st(supplies.begin(), supplies.end());
+        unordered_map<string, vector<string>> mp;
+        unordered_map<string, int> visited;
+        vector<string> res;
+
+        for (size_t i = 0; i < recipes.size(); i++)
+        {
+            mp[recipes[i]] = ingredients[i];
+        }
+        function<bool(string)> check = [&](string recipe)
+        {
+            if (visited.count(recipe))
+            {
+                return visited[recipe] == 1;
+            }
+            if (st.count(recipe))
+            {
+                return true;
+            }
+
+            if (!mp.count(recipe))
+            {
+                return false;
+            }
+            visited[recipe] = 0;
+            for (const string &ingredient : mp[recipe])
+            {
+                if (!check(ingredient))
+                {
+                    visited[recipe] = -1;
+                    return false;
+                }
+            }
+
+            visited[recipe] = 1;
+            res.push_back(recipe);
+            return true;
+        };
+
+        for (const string &recipe : recipes)
+        {
+            check(recipe);
+        }
+
+        return res;
+    }
+};
