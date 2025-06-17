88391ec23c829f9d93e59d40ee9abf1b430dc271
Arpita Agrahari <arpitaagrahari004@gmail.com>
2025-06-17 13:13:40 +0530

    Refactor code structure and update main functions for palindrome and GCD calculations

--- summer-pep/Day2.cpp
+++ summer-pep/Day2.cpp
@@ -1,73 +1,73 @@
 
 // check wheatheer Ip address is valid or not
 
-// #include <iostream>
-// #include <string>
-// #include <sstream>
-// using namespace std;
+#include <iostream>
+#include <string>
+#include <sstream>
+using namespace std;
 
-// bool isValidIP(string s)
-// {
-//     int count = 0;
-//     stringstream ss(s);
-//     string part;
-//     while (getline(ss, part, '.'))
-//     {
-//         if (part.empty() || part.size() > 3)
-//             return false;
-//         for (char c : part)
-//         {
-//             if (!isdigit(c))
-//                 return false;
-//         }
-//         int num = stoi(part);
-//         if (num < 0 || num > 255)
-//             return false;
-//         if (part[0] == '0' && part.size() > 1)
-//             return false;
-//         count++;
-//     }
-//     return count == 4;
-// }
+bool isValidIP(string s)
+{
+    int count = 0;
+    stringstream ss(s);
+    string part;
+    while (getline(ss, part, '.'))
+    {
+        if (part.empty() || part.size() > 3)
+            return false;
+        for (char c : part)
+        {
+            if (!isdigit(c))
+                return false;
+        }
+        int num = stoi(part);
+        if (num < 0 || num > 255)
+            return false;
+        if (part[0] == '0' && part.size() > 1)
+            return false;
+        count++;
+    }
+    return count == 4;
+}
 
-// int main()
-// {
-//     string s;
-//     cin >> s;
-//     if (isValidIP(s))
-//     {
-//         cout << "True";
-//     }
-//     else
-//     {
-//         cout << "False";
-//     }
-// }
+int main()
+{
+    string s;
+    cin >> s;
+    if (isValidIP(s))
+    {
+        cout << "True";
+    }
+    else
+    {
+        cout << "False";
+    }
+}
 
 // encrypiton and decryption in CPP
 
-// #include <iostream>
-// using namespace std;
+#include <iostream>
+using namespace std;
 
-// string encrypt(string s, string key)
-// {
-//     string output = s;
-//     for (int i = 0; i < s.size(); ++i)
-//     {
-//         output[i] = s[i] ^ key[i - key.size()];
-//     }
-//     return output;
-// }
-// int main()
-// {
-//     string s;
-//     cin >> s;
-//     string key = "abcdefghijklmn";
-//     string en = encrypt(s, key);
-//     cout << en << endl;
-//     string de = encrypt(en, key);
-//     cout << de << endl;
-// }
+string encrypt(string s, string key)
+{
+    string output = s;
+    for (int i = 0; i < s.size(); ++i)
+    {
+        output[i] = s[i] ^ key[i - key.size()];
+    }
+    return output;
+}
+int main()
+{
+    string s;
+    cin >> s;
+    string key = "abcdefghijklmn";
+    string en = encrypt(s, key);
+    cout << en << endl;
+    string de = encrypt(en, key);
+    cout << de << endl;
+}
 
 // to make a sentence a palidrome leeter to claculate ; convert innot palidrome
 // ex malayal   o/p malayalam and cnt 2 adding am
@@ -103,4 +103,33 @@ int main()
             break;
         }
     }
+}
+
+// input 2 number find gcd
+#include <bits/stdc++.h>
+using namespace std;
+int findgcd(int a, int b)
+{
+    if (b == 0)
+        return a;
+    return findgcd(b, a % b);
+}
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    int res = findgcd(a, b);
+    cout << res << endl;
+}
+
+// using built in
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    int res = __gcd(a, b);
+    cout << res << endl;
 }
