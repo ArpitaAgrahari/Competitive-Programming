243713a47381950b0931822410a23ed58b82a2c0
Arpita Agrahari <arpitaagrahari004@gmail.com>
2025-06-17 13:36:02 +0530

    Implement Sieve of Eratosthenes algorithm for prime counting in Day2.cpp

--- summer-pep/Day2.cpp
+++ summer-pep/Day2.cpp
@@ -133,3 +133,41 @@ int main()
     int res = __gcd(a, b);
     cout << res << endl;
 }
+
+// seive of eratosthenes
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int seieve(int a, int b)
+{
+    vector<bool> isPrime(b + 1, true);
+    isPrime[0] = isPrime[1] = false;
+
+    for (int i = 2; i * i <= b; i++)
+    {
+        if (isPrime[i])
+        {
+            for (int j = i * i; j <= b; j += i)
+                isPrime[j] = false;
+        }
+    }
+
+    int count = 0;
+    for (int i = a; i <= b; i++)
+    {
+        if (isPrime[i])
+            count++;
+    }
+
+    return count;
+}
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    int res = seieve(a, b);
+    cout << res << endl;
+}
