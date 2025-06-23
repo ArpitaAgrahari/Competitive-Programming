b0e830331babdc852857f2511a32f4de0224d352
Arpita Agrahari <arpitaagrahari004@gmail.com>
2025-06-23 09:58:52 +0530

    Implement maximum subarray function with constraint k in Day3.cpp

--- summer-pep/Day3.cpp
+++ summer-pep/Day3.cpp
@@ -1,5 +1,34 @@
 // 1. maximum subarray < k
 
+#include <bits/stdc++.h>
+using namespace std;
+int maximumsubarray(int arr[], int n, int k)
+{
+    int maxSum = INT_MIN;
+    for (int i = 0; i < n; i++)
+    {
+        int sum = 0;
+        for (int j = i; j < n; j++)
+        {
+            sum += arr[j];
+            if (sum < k)
+                maxSum = max(maxSum, sum);
+        }
+    }
+    return (maxSum == INT_MIN) ? -1 : maxSum;
+}
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    cout << maximumsubarray(arr, n);
+}
+
 // 2. kadane algo
 #include <bits/stdc++.h>
 using namespace std;
